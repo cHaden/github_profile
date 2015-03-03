b323d5a14e7410ae87453048f8800b0f29e4a8bf
C Haden <chaden@spoonflower.com>
2015-03-03 00:26:44 -0500

    starting to add info from API

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,4 +1,15 @@
 class GithubController < ApplicationController
   def index
+    @user = HTTParty.get("https://api.github.com/users/cHaden",
+      :headers => {
+        "Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+        "User-Agent" => "cHaden"
+        })
+
+    @response = HTTParty.get("https://api.github.com/users/cHaden/events",
+      :headers => {
+        "Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+        "User-Agent" => "cHaden"
+        })
   end
 end
